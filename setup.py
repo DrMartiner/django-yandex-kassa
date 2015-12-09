52eb6de3649d3916e8bde3029fd9cfa323d714ed
drmartiner <DrMartiner@gmail.com>
2015-12-09 12:17:11 +0600

    "version 0.9.3"

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 from distutils.core import setup
 
 __author__ = 'Alexei Kuzmin'
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
