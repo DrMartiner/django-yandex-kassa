8364db85e4da84ecff3ea72333d482d8e4af58b3
drmartiner <DrMartiner@gmail.com>
2015-12-17 15:57:50 +0600

    [kassa] chore: added payment type QiWi

--- yandex_kassa/models.py
+++ yandex_kassa/models.py
@@ -35,6 +35,7 @@ class Payment(models.Model):
         PC = 'pc'
         SB = 'sb'
         WM = 'wm'
+        QW = 'qw'
 
         CHOICES = (
             (AB, u'Альфа-Клик'),
@@ -46,6 +47,7 @@ class Payment(models.Model):
             (PC, u'Кошелек Яндекс.Денег'),
             (SB, u'Сбербанк Онлайн'),
             (WM, u'Кошелек WebMoney'),
+            (QW, u'QiWi кошелёк'),
         )
 
     class CURRENCY:
