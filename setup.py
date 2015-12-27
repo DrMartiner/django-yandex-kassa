dd8616a1de34b331b79405d7f8bafca8deaa0eab
drmartiner <DrMartiner@gmail.com>
2015-12-27 17:41:03 +0600

    "version 0.9.9"

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 from setuptools import find_packages
 
 __author__ = 'Alexei Kuzmin'
-__version__ = "0.9.8"
+__version__ = "0.9.9"
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
