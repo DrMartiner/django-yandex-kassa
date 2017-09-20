ad19c62566c946e9a18a3f0089b234f0fbd00e9f
Alexei Kuzmin <DrMartiner@GMail.Com>
2017-09-20 10:59:17 +0700

    #14 Added "S" at the end to YANDEX_KASSA_PAYMENT_TYPE

--- yandex_kassa/conf.py
+++ yandex_kassa/conf.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
 import ast
+import six
+import logging
 from django.conf import settings
 
-import six
+logger = logging.getLogger('kassa')
+
 
 DEBUG = getattr(settings, 'YANDEX_KASSA_DEBUG', False)
 if isinstance(DEBUG, six.string_types[0]):
@@ -20,4 +23,12 @@ CALLBACK_URL = getattr(settings, 'YANDEX_KASSA_CALLBACK_URL', '/kassa/callback/'
 FAIL_URL = getattr(settings, 'YANDEX_KASSA_FAIL_URL', '/kassa/fail/')
 SUCCESS_URL = getattr(settings, 'YANDEX_KASSA_SUCCESS_URL', '/kassa/success/')
 
-PAYMENT_TYPES = getattr(settings, 'YANDEX_KASSA_PAYMENT_TYPE', ['AB', 'AC', 'GP', 'PB', 'PC', 'WM'])
+PAYMENT_TYPES = getattr(settings, 'YANDEX_KASSA_PAYMENT_TYPE')
+
+if PAYMENT_TYPES:
+    msg = 'YANDEX_KASSA_PAYMENT_TYPE was deprecated. Rename it to YANDEX_KASSA_PAYMENT_TYPES'
+    logger.warning(msg)
+else:
+    PAYMENT_TYPES = getattr(settings, 'YANDEX_KASSA_PAYMENT_TYPES')
+if not PAYMENT_TYPES:
+    PAYMENT_TYPES = ['AB', 'AC', 'GP', 'PB', 'PC', 'WM']
